0a871bd6a56bb8941f3e9a936a0a2622f3fafa22
andritowmega <andrew_c_q@hotmail.com>
2020-07-27 14:00:23 -0500

    Config

---
+++ app/src/main/java/pe/smarttech/ogame/ServersActivity.java
@@ -0,0 +1,14 @@
+package pe.smarttech.ogame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ServersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_servers);
+    }
+}
